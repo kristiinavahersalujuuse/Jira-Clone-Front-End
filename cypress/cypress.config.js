43da3507ad4707d67b45fd6843354f60437dabf2
kristiinavahersalujuuse <kristiina.vahersalu@gmail.com>
2024-07-29 12:33:31 +0300

    My first commit

---
+++ cypress/cypress.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  projectId: 'ddyz5f',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
--- cypress/e2e/authentication-tests.cy.js
+++ cypress/e2e/authentication-tests.cy.js
@@ -1,19 +1,17 @@
-describe('Authentication and verifying default setup', () => {
+describe("Authentication and verifying default setup", () => {
   beforeEach(() => {
-    cy.visit('/project/board');
+    cy.visit("/project/board");
   });
 
-  it('Should create guest account if user has no auth token and verify default amount of issues on board', () => {
-    cy.window()
-      .its('localStorage.authToken')
-      .should('be.undefined');
+  it("Should create guest account if user has no auth token and verify default amount of issues on board", () => {
+    cy.window().its("localStorage.authToken").should("be.undefined");
 
     cy.window()
-      .its('localStorage.authToken')
-      .should('be.a', 'string')
-      .and('not.be.empty');
+      .its("localStorage.authToken")
+      .should("be.a", "string")
+      .and("not.be.empty");
 
-    //Assert total amount of issues on the board  
-    cy.get('[data-testid="list-issue"]').should('have.length', 8);
+    //Assert total amount of issues on the board
+    cy.get('[data-testid="list-issue"]').should("have.length", 8);
   });
 });
