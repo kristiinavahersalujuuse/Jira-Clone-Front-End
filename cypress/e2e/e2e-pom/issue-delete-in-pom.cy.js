bf79849a0cf6e2dac6a950e4b29560f13da14184
kristiinavahersalujuuse <kristiina.vahersalu@gmail.com>
2024-08-05 13:28:49 +0300

    Adding a comment to delete issue pom file

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -1,6 +1,21 @@
 /**
  * This is an example file and approach for POM in Cypress
  */
+
+/* In order to run this code succesfully add following function to IssueModal.js file
+
+ensureNrOfIssuesAfterDel(expectedAmountIssues, issueTitle) {
+    cy.get(this.backlogList)
+      .should('be.visible')
+      .and('have.length', '1')
+      .within(() => {
+        cy.get(this.issuesList).should('have.length', expectedAmountIssues);
+      });
+    cy.get(this.backlogList).contains(issueTitle).should('not.exist');
+  }
+
+*/
+
 import IssueModal from '../../pages/IssueModal';
 
 // Variables
